41a9cb6dc45222441e85db42aa4d1facc1eaa099
yusufazgea <yusufazgea@gmail.com>
2023-06-20 22:03:13 +0700

    pbwl-tugas2
    Tugas 2 SI-MVC Pemrograman Berbasis Web Lanjutan 2023

---
+++ app/views/dashboard/index.php
@@ -0,0 +1,3 @@
+<h2>Dashboard</h2>
+
+<div class="info">Selamat datang, <strong>Yusuf Abdul Aziz Gea</strong></div>
---
+++ app/views/index/index.php
@@ -0,0 +1,7 @@
+<h2>Selamat Datang</h2>
+
+<div class="info">    
+    <h3>
+        <a style="color: blue; text-decoration: none" href="<?php echo URL; ?>/log/">Silahkan login</a>
+    </h3>
+</div>
